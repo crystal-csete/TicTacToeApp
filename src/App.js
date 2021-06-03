7fe1ab65153b18dfbec324e268b0687b475d58cf
Crystal Csete <jeannecc13@gmail.com>
2021-06-02 18:15:04 -0600

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      player_turn: "X",
+      board: ["", "", "", "", "", "", "", "", ""],
+    };
+
+    this.squareClicked = this.squareClicked.bind(this);
+  }
+
+  squareClicked(index) {
+    let player_turn = this.state.player_turn;
+    let board = this.state.board;
+
+    // console.log("index", index);
+
+    board[index] = player_turn;
+    let winning_combos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winning_combos.length; i++) {
+      let winning_row = winning_combos[i];
+      let p1 = winning_row[0];
+      let p2 = winning_row[1];
+      let p3 = winning_row[2];
+      if (board[p1] === board[p2] && board[p2] && board[p3] === board[p1]) {
+        alert(`Winner: player ${player_turn} has won the game`);
+      }
+    }
+
+    player_turn = player_turn === "X" ? "O" : "X";
+    // console.log("player_turn", player_turn);
+    this.setState({
+      player_turn: player_turn,
+      board: board,
+    });
+  }
+
+  render() {
+    return (
+      <div className='app__container'>
+        <h1>Tic Tac Toe</h1>
+        <div className='board'>
+          {this.state.board.map((square, index) => {
+            return (
+              <div onClick={() => this.squareClicked(index)} className='square'>
+                <h3 className='symbol'>{square}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
